760083d33d7cc8031392531fbf6cd85885a7d518
Balasubramaniyan R <61647258+SnthoshBala@users.noreply.github.com>
2020-07-20 21:02:16 +0530

    Update BookController.java

--- src/main/java/com/spring/bookmongo/resource/BookController.java
+++ src/main/java/com/spring/bookmongo/resource/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @CrossOrigin(origins="https://booklist-bala.herokuapp.com")
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 public class BookController {
     
